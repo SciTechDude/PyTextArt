c328e805286a85cbdc4b9ddfac3ddca3aea53809
Anil Mandra <Anil.Mandra@acxiom.com>
2017-11-02 11:37:51 -0500

    Fixed hollow attribute boolean logic

--- bowTieShapeGenerator.py
+++ bowTieShapeGenerator.py
@@ -10,28 +10,39 @@ from __future__ import print_function
 from itertools import islice, chain
 
 class BowTie(object):
-    def __init__(self, size=5, fill_value = '*', empty_value = ' '):
+    def __init__(self, size=5, fill_value = '*', empty_value = ' ', hollow = None):
         self.size = size
         self.fill_value = fill_value
         self.empty_value = empty_value
+        self.hollow = hollow
+        
 
     def  create_shape(self):
         ''' creates shape as per input values and returns list of list of values'''
         face = [self.fill_value,self.empty_value] * ((self.size*2) - 1)
         rows = list(chain(range(self.size),reversed(range(self.size-1))))
         for i in rows:
-            l1 = ''.join(chain(islice(face, i, (i*2)+1) , [self.empty_value * (self.size - (i + 1))]))
-            yield ''.join(l1 + l1[::-1][1:])
+            f = ''.join(islice(face, i, (i*2)+1))            
+            if not self.hollow:
+                s = ''.join(chain(f , [self.empty_value * (self.size - (i + 1))]))                
+            else:
+                self.empty_value = ' '
+                l = [self.fill_value if idx == 0 or  idx == len(f)-1 else self.empty_value for idx,m in enumerate(f)]
+                s = ''.join(chain(l, [self.empty_value * (self.size - (i + 1))]))
+                
+            yield ''.join(s + s[::-1][1:])
 
     def print_shape(self):
         ''' print given list of list of values'''
-        for i in self.create_shape():
+        for i in self.create_shape():            
             print(i)
 
 
 #example use 1
-BowTie(size=5, fill_value = '*', empty_value = ' ').print_shape()
+BowTie(size=5, fill_value = '*', empty_value = '.', hollow = False).print_shape()
+
+#example use 1
+BowTie(size=7, fill_value = '#', empty_value = ' ', hollow = False).print_shape()
 
 #example use 2
-BowTie(size=16, fill_value = '#', empty_value = '.').print_shape()
-    
+BowTie(size=10, fill_value = '+', empty_value = '.', hollow = True).print_shape()
